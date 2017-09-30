c45eb14e6547f133249fbbb89f8f03479e8b94b1
cardboardcode <cardboardmails@gmail.com>
2017-09-30 14:25:31 +0800

    updated codes used for hardware testing

--- hardware/accelero/accelero.ino
+++ hardware/accelero/accelero.ino
@@ -1,58 +1,60 @@
-//connect 3.3v to AREF
+/*
+  ADXL3xx
 
+  Reads an Analog Devices ADXL3xx accelerometer and communicates the
+  acceleration to the computer. The pins used are designed to be easily
+  compatible with the breakout boards from SparkFun, available from:
+  http://www.sparkfun.com/commerce/categories.php?c=80
 
-const int ap1 = A5; 
-const int ap2 = A4;
-const int ap3 = A3;
+  The circuit:
+  - analog 0: accelerometer self test
+  - analog 1: z-axis
+  - analog 2: y-axis
+  - analog 3: x-axis
+  - analog 4: ground
+  - analog 5: vcc
 
-int sv1 = 0;        
-int ov1 = 0;    
-int sv2 = 0;      
-int ov2= 0;      
-int sv3 = 0;       
-int ov3= 0;      
+  created 2 Jul 2008
+  by David A. Mellis
+  modified 30 Aug 2011
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/ADXL3xx
+*/
+
+// these constants describe the pins. They won't change:
+const int groundpin = 18;             // analog input pin 4 -- ground
+const int powerpin = 19;              // analog input pin 5 -- voltage
+const int xpin = A3;                  // x-axis of the accelerometer
+const int ypin = A2;                  // y-axis
+const int zpin = A1;                  // z-axis (only on 3-axis models)
 
 void setup() {
-  // initialize serial communications at 9600 bps:
+  // initialize the serial communications:
   Serial.begin(9600);
- 
+
+  // Provide ground and power by using the analog inputs as normal digital pins.
+  // This makes it possible to directly connect the breakout board to the
+  // Arduino. If you use the normal 5V and GND pins on the Arduino,
+  // you can remove these lines.
+  pinMode(groundpin, OUTPUT);
+  pinMode(powerpin, OUTPUT);
+  digitalWrite(groundpin, LOW);
+  digitalWrite(powerpin, HIGH);
 }
 
 void loop() {
-  analogReference(EXTERNAL);    //connect 3.3v to AREF
-  // read the analog in value:
-  sv1 = analogRead(ap1);            
-  // map it to the range of the analog out:
-  ov1 = map(sv1, 0, 1023, 0, 255);  
-  // change the analog out value:
-  delay(2);                     
-  //
-  sv2 = analogRead(ap2);            
-  
-  ov2 = map(sv2, 0, 1023, 0, 255); 
- // 
-  delay(2);                 
-  //
-  sv3 = analogRead(ap3);            
-  
-  ov3 = map(sv3, 0, 1023, 0, 255);  
-  
-  // print the results to the serial monitor:
-  Serial.print("Xsensor1 = " );                       
-  Serial.print(sv1);      
-  Serial.print("\t output1 = ");      
-  Serial.println(ov1);   
- 
-  Serial.print("Ysensor2 = " );                       
-  Serial.print(sv2);      
-  Serial.print("\t output2 = ");      
-  Serial.println(ov2);   
-
-  Serial.print("Zsensor3 = " );                       
-  Serial.print(sv3);      
-  Serial.print("\t output3 = ");      
-  Serial.println(ov3);   
-
-  delay(3000);                     
-  
+  // print the sensor values:
+  Serial.print(analogRead(xpin));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(ypin));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(zpin));
+  Serial.println();
+  // delay before next reading:
+  delay(100);
 }
