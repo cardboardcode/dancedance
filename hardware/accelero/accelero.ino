aa95eef0fb55a34adeda922d4faf42422aa91b02
cardboardcode <cardboardmails@gmail.com>
2017-10-06 00:53:17 +0800

    added new accelerometer measurement codes. now in g force

--- hardware/accelero/accelero.ino
+++ hardware/accelero/accelero.ino
@@ -1,240 +1,158 @@
+/*
+ADXL335 Accelerometer Demo
+Derek Chafin
+September 22, 2011
+Public Domain
 
-// these constants describe the pins. They won't change:
-const int xpin = A6;                  // x-axis of the accelerometer
-const int ypin = A7;                  // y-axis
-const int zpin = A8;                  // z-axis (only on 3-axis models)
+Demonstrates how to use the ADXL335 library
 
-struct accelero{
+Pin Connections:
 
-  int x;
-  int y;
-  int z;
-  
-  };
+analog 0: connected to x-axis
+analog 1: connected to y-axis
+analog 2: connected to z-axis
+aref: connected to +3.3V
+*/
 
-double value_window_x[10];
-double value_window_y[10];
-double value_window_z[10];
+#include <ADXL335.h>
 
-double average_window_x[10];
-double average_window_y[10];
-double average_window_z[10];
+const int pin_x = A0;
+const int pin_y = A1;
+const int pin_z = A2;
+const float aref = 3.3;
+ADXL335 accel(pin_x, pin_y, pin_z, aref);
 
-struct accelero a1;
-
-float average_x;
-float average_y;
-float average_z;
-
-float super_average_x;
-float super_average_y;
-float super_average_z;
-
-float idle_current_x = 0.0;
-float idle_prev_x = 0.0;
-float idle_current_y = 0.0;
-float idle_prev_y = 0.0;
-float idle_current_z = 0.0;
-float idle_prev_z = 0.0;
-
-void initAccel(void);
-void initWindow(void);
-void updateWindow(void);
-void updateAverage(void);
-void getAverage(void);
-
-void initAccel(){
-  a1.x = xpin;
-  a1.y = ypin;
-  a1.z = zpin;
-  
-  }
-
-
-void initWindow(){
-
-  for (int i = 0; i < 10; i++){
-    
-    value_window_x[i] = analogRead(a1.x);
-    value_window_y[i] = analogRead(a1.y);
-    value_window_z[i] = analogRead(a1.z);
-    
-  }
-  
-}
-
-void updateWindow(){
-
-   //Push all values toward the left to make space for the new value
-   for (int i = 1; i < 10 ;i++){
-
-      value_window_x[i-1] = value_window_x[i];
-      value_window_y[i-1] = value_window_y[i];
-      value_window_z[i-1] = value_window_z[i];
-    
-   }
-
-  //Insert new value at the end of the value_window
-
-  double diff_x = analogRead(a1.x) - value_window_x[9];
-  double diff_y = analogRead(a1.y) - value_window_y[9];
-  double diff_z = analogRead(a1.z) - value_window_z[9];
-  if (diff_x > 5 || diff_x < -5){
-  value_window_x[9] = analogRead(a1.x);
-  }
-  if (diff_y > 5 || diff_y < -5){
-  value_window_y[9] = analogRead(a1.y);
-  }
-  if (diff_z > 5 || diff_z < -5){
-  value_window_z[9] = analogRead(a1.z);
-  }
-  
-  delay(20);
-  
-}
-
-void setup() {
-  // initialize the serial communications:
+void setup()
+{
   Serial.begin(9600);
-
-  initAccel();
-
-  initWindow();
   
+  Serial.println("X,\tY,\tZ,\tRho,\tPhi,\tTheta");
 }
 
-void getAverage(){
-
-  double local_x;
-  double local_y;
-  double local_z;
+void loop()
+{
+  //this is required to update the values
+  accel.update();
   
-  for (int i = 0; i < 10; i++){
-    local_x += value_window_x[i];
-    local_y += value_window_y[i];
-    local_z += value_window_z[i];   
+  //this tells us how long the string is
+  int string_width;
+
+  float x;
+  float y;
+  float z;
+  
+  //for these variables see wikipedia's
+  //definition of spherical coordinates
+  float rho;
+  float phi;
+  float theta;  
+
+  accel.setThreshold(0.5);
+  
+  x = accel.getX();
+  y = accel.getY();
+  //if the project is laying flat and top up the z axis reads ~1G
+  z = accel.getZ();
+  rho = accel.getRho();
+  phi = accel.getPhi();
+  theta = accel.getTheta();
+  
+  Serial.print(formatFloat(x, 2, &string_width));
+  Serial.print(",\t");
+  Serial.print(formatFloat(y, 2, &string_width));
+  Serial.print(",\t");
+  Serial.print(formatFloat(z, 2, &string_width));
+  Serial.print(",\t");
+  Serial.print(formatFloat(rho, 2, &string_width));
+  Serial.print(",\t");
+  Serial.print(formatFloat(phi, 2, &string_width));
+  Serial.print(",\t");
+  Serial.print(formatFloat(theta, 2, &string_width));
+  Serial.println("");
+  
+  delay(100);
+}
+
+//this function was taken from my format float library
+String formatFloat(double value, int places, int* string_width)
+{
+  //if value is positive infinity
+  if (isinf(value) > 0)
+  {
+    return "+Inf";
   }
-
-  average_x = local_x/10;
-  average_y = local_y/10;
-  average_z = local_z/10;
-  
-}
-
-
-double getSuperAverage(){
-
-  double local_x;
-  double local_y;
-  double local_z;
-  
-  for (int i = 0; i < 10; i++){
-    local_x += average_window_x[i];
-    local_y += average_window_y[i];
-    local_z += average_window_z[i];   
+    
+  //Arduino does not seem to have negative infinity
+  //keeping this code block for reference
+  //if value is negative infinity
+  if(isinf(value) < 0)
+  {
+    return "-Inf";
   }
-
-  super_average_x = local_x/10;
-  super_average_y = local_y/10;
-  super_average_z = local_z/10;
-
-}
-
-
-void updateAverage(){
-
-   //Push all values toward the left to make space for the new value
-   for (int i = 1; i < 10 ;i++){
-
-      average_window_x[i-1] = average_window_x[i];
-      average_window_y[i-1] = average_window_y[i];
-      average_window_z[i-1] = average_window_z[i];
-    
-   }
-     
-   average_window_x[9] = average_x;
-   average_window_y[9] = average_y;
-   average_window_z[9] = average_z; 
-}
-
-boolean isIdle(){
-
-    int compare_x = average_window_x[0];
-    int compare_y = average_window_y[0];
-    int compare_z = average_window_z[0];
-    
-    for (int i = 1; i < 10; i++){
-      if (average_window_x[i] != compare_x){
-          return false;
-        }
-      if (average_window_y[i] != compare_y){
-          return false;
-        }
-      if (average_window_z[i] != compare_z){
-          return false;
-        }
-      }
-    return true;
-    
-}
-
-boolean isStationary(){
-
-     if (idle_current_x - idle_prev_x > 2)
-        return false;
-     if (idle_current_y - idle_prev_y > 2)
-        return false;
-     if (idle_current_y - idle_prev_y > 2)
-        return false;
-
-     if (idle_current_x - idle_prev_x < -2)
-        return false;
-     if (idle_current_y - idle_prev_y < -2)
-        return false;
-     if (idle_current_z - idle_prev_z < -2)
-        return false;  
-
-      return true;
+  
+  //if value is not a number
+  if(isnan(value) > 0)
+  {
+    return "NaN";
   }
-
-void loop() {
   
-  updateWindow();
+  //always include a space for the dot
+  int num_width = 1;
 
-  getAverage();
-
-  updateAverage();
+  //if the number of decimal places is less than 1
+  if (places < 1)
+  {
+    //set places to 1
+    places = 1;
+    
+    //and truncate the value
+    value = (float)((int)value);
+  }
   
-  getSuperAverage();
+  //add the places to the right of the decimal
+  num_width += places;
   
-  if(isIdle){
-    idle_current_x = super_average_x;
-    idle_current_y = super_average_y;
-    idle_current_z = super_average_z;
-    }
+  //if the value does not contain an integral part  
+  if (value < 1.0 && value > -1.0)
+  {
+    //add one for the integral zero
+    num_width++;
+  }
+  else
+  {
+
+    //get the integral part and
+    //get the number of places to the left of decimal
+    num_width += ((int)log10(abs(value))) + 1;
+  }
+  //if the value in less than 0
+  if (value < 0.0)
+  {
+    //add a space for the minus sign
+    num_width++;
+  }
   
-  if (idle_current_x - idle_prev_x > 2)
-      Serial.println("RIGHT ------------------------->");
-  else if (idle_current_x - idle_prev_x < -2)
-      Serial.println("LEFT-------------------------<");
-
-  if (idle_current_y - idle_prev_y > 2)
-      Serial.println("DOWN ------------------------->");
-  else if (idle_current_y - idle_prev_y < -2)
-      Serial.println("UP-------------------------<");
-
-  if (idle_current_z - idle_prev_z > 2)
-      Serial.println("FRONT ------------------------->");
-  else if (idle_current_z - idle_prev_z < -2)
-      Serial.println("BACK-------------------------<");
-
-
-//   if (isStationary)
-//      Serial.println("NOT MOVING");
+  //make a string the size of the number
+  //plus 1 for string terminator
+  char s[num_width + 1]; 
   
-  idle_prev_x = idle_current_x;
-  idle_prev_y = idle_current_y;
-  idle_prev_z = idle_current_z;
+  //put the string terminator at the end
+  s[num_width] = '\0';
   
-  delay(50);
+  
+  //initalize the array to all zeros
+  for (int i = 0; i < num_width; i++)
+  {
+    s[i] = '0';
+  }
+  
+  //characters that are not changed by 
+  //the function below will be zeros
+  
+  //set the out variable string width
+  //lets the caller know what we came up with
+  *string_width = num_width;
+  
+  //use the avr-libc function dtosrtf to format the value
+  return String(dtostrf(value,num_width,places,s));  
 }
