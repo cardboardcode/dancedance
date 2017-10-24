44973a98f4c3a77904e27cb312aba64a36af5c69
ZYZZYVA\Sidai <c13413968687@gmail.com>
2017-10-24 12:52:04 +0800

    Fast version of comm communication

--- hardware/voltage_sensor/voltage_sensor.ino
+++ hardware/voltage_sensor/voltage_sensor.ino
@@ -31,7 +31,7 @@ void loop()
 {
     // take a number of analog samples and add them up
     while (sample_count < NUM_SAMPLES) {
-        sum += analogRead(A2);
+        sum += analogRead(A12);
         sample_count++;
         delay(10);
     }
