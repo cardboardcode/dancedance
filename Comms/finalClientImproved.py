42e25d3232b1cd0773fa77508088a51bff77d2bf
dheerajaraj <dheerajaraj@yahoo.com.sg>
2017-10-30 20:04:17 +0800

    ClientIntegration + CumPower

--- Comms/finalClientImproved.py
+++ Comms/finalClientImproved.py
@@ -4,39 +4,45 @@ import hashlib
 import base64
 import socket
 import time
+import sys
 #import pandas as pd
 import os
 import csv
 
-HOST = "192.168.43.81" ##o or 172.20.10.4
-PORT = 4957
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-bs = 32
-raw_key = "1234567890123456"
-key = hashlib.sha256(raw_key.encode()).digest()
-x = 0
-f = open('TestData.txt','r')
+class ClientCommunication:
+	def__init__(self,predAction,voltage,current,power,cumpower,host, port):
+		self.HOST = host#"172.20.10.2"
+		self.PORT = port#4957
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.s.connect((HOST, PORT))
+		self.bs = 32
+		self.key = "1234567890123456"
+		self.predAction = predAction
+		self.voltage = voltage
+		self.current = current
+		self.power = power
+		self.cumpower=cumpower
+		self.formattedplaintext = "#"
 
-def encryptText(plainText, key):
-    raw = pad(plainText)
-    iv = Random.new().read(AES.block_size) #cryptographically secured keys as opposed to pseudorandome in random class
-    cipher = AES.new(key,AES.MODE_CBC,iv)
-    return base64.b64encode(iv+cipher.encrypt(raw))
+	def encryptText(self, plainText, key):
+	    raw = pad(plainText)
+	    iv = Random.new().read(AES.block_size) #cryptographically secured keys as opposed to pseudorandome in random class
+	    cipher = AES.new(key,AES.MODE_CBC,iv)
+	    return base64.b64encode(iv+cipher.encrypt(raw))
 
-def pad(var1):
-    return var1 + (bs - len(var1)%bs)*chr(bs - len(var1)%bs)
+	def pad(self, var1):
+	    return var1 + (self.bs - len(var1)%self.bs)*chr(self.bs - len(var1)%self.bs)
 # df = pd.read_csv('storageData.csv')
 
-while (x<49):	
-	plainText = f.readline()
-	var1= str(plainText)
-	var1 = var1.strip();
-	finalString = encryptText(var1,key)
-	x=x+1
-	s.send(finalString)
-	time.sleep(1)
+	def formatPlainText(self):
+		self.formattedplaintext +=(self.predAction + "|"+self.voltage+"|"+self.current+"|"+self.power+"|"+self.cumpower+"|") 
 
-s.close()
+	def sendData(self, formattedplaintext):
+		formatPlainText()
+		finalString = encryptText(self.formattedplaintext)
+		s.send(finalString)
+		time.sleep(1)
+
+		s.close()
 
 
